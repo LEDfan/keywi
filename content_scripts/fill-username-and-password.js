0d3e59b755ec9036e3437eb17a20cf8efde0d28b
LEDfan <tobia@ledfan.be>
2017-02-07 08:10:53 +0100

    Only fill username when no password field can be found   and show a notification

--- content_scripts/fill-username-and-password.js
+++ content_scripts/fill-username-and-password.js
@@ -26,10 +26,76 @@ function searchForPasswordInput(userInput) {
     }
 }
 
+
+/**
+ * Shows a notification to the user in the right top corner. Will be removed after 3 seconds.
+ * @todo animations
+ * @param id the html id, is automatically prefixed with "keepass_msg_".
+ * @param msg the message to show to the user
+ */
+function showMessage(id, msg) {
+    document.body.insertAdjacentHTML('afterbegin', `
+<style>
+.keepass_title {
+
+}
+
+.keepass_msg_text {
+
+}
+
+.keepass_message-container {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    width: 300px;
+    height: 100px;
+    background-color: #FFFFFF;
+    z-index: 1000;
+    
+    border: 1px solid #636363;
+    border-radius: 4px;
+    box-shadow: 0 1px 1px rgba(0,0,0,.05);
+}
+
+.keepass_message-heading {
+    color: #333;
+    background-color: #ddd;
+    padding: 10px 15px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    border-bottom: 1px solid #636363;
+}
+
+.keepass_message-body {
+    padding: 15px;
+}
+
+</style>
+<div id="keepass_msg_` + id + `" class="keepass_message-container">
+    <div class="keepass_message-heading">
+        <span class="keepass_title">Keepass</span>
+    </div>
+    <div class="keepass_message-body">
+        <p class="keepass_msg_text">` + msg  + `</p>
+    </div>
+</div>
+`);
+
+    var element = document.getElementById("keepass_msg_" + id);
+    setInterval(function () {
+        element.parentElement.removeChild(element);
+    }, 3000);
+}
+
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     var usernameField = document.activeElement;
-    var passwordField = searchForPasswordInput(usernameField);
     usernameField.value = request.username;
-    passwordField.value = request.password;
+    var passwordField = searchForPasswordInput(usernameField);
+    if (passwordField === null) {
+        showMessage("no-password", "No password field found, only filling username.");
+    } else {
+        passwordField.value = request.password;
+    }
 });
 
