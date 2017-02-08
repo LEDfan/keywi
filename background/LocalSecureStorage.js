01b47da3b0627598e72b28f13e490445fd12deb6
LEDfan <tobia@ledfan.be>
2017-02-08 08:24:07 +0100

    Only aks the user once for a key

--- background/LocalSecureStorage.js
+++ background/LocalSecureStorage.js
@@ -3,6 +3,74 @@ function LocalSecureStorage() {
 
 LocalSecureStorage.prototype = new SecureStorage();
 
+LocalSecureStorage.prototype._encryptionkey = null;
+
+LocalSecureStorage.prototype._hasEncryptionKey = function (onHas, onHasNot) {
+    /**
+     * We store some dummy data in the LocalSecureStorage to make the user already provided an encryption key.
+     */
+    this.has("local_secure_storage_encryption_key_test", onHas, onHasNot);
+};
+
+LocalSecureStorage.prototype._unlockStorage = function (onSuccess, onError) {
+    // first check if there is already an encryption key
+    if (LocalSecureStorage.prototype._encryptionkey !== null) {
+        onSuccess();
+    } else {
+        // check if the user has ever provided an encryption key
+        this._hasEncryptionKey(function () {
+            // we have an encryption key, ask the user to input this, and verify that this is the correct key and store it for later user
+            confirmFromBg("Fill in the existing password for the secure storage.. (TODO)", function (encryptionKey) {
+                browser.storage.local.get("local_secure_storage_encryption_key_test").then(function (data) {
+                    var actualData = data["local_secure_storage_encryption_key_test"];
+                    var iv = actualData.nonce;
+                    var verifier = actualData.verifier;
+
+                    /**
+                     * First verify that the provided key to the LocalSecureStorage is correct.
+                     */
+                    var checkIv = slowAES.decrypt(
+                        cryptoHelpers.convertStringToByteArray(atob(verifier)),
+                        slowAES.modeOfOperation.CBC,
+                        cryptoHelpers.convertStringToByteArray(atob(encryptionKey)),
+                        cryptoHelpers.convertStringToByteArray(atob(iv))
+                    );
+
+                    var checkIvStr = cryptoHelpers.convertByteArrayToString(checkIv);
+
+                    if (checkIvStr !== iv) {
+                        console.log("Error decrypting: key wrong!");
+                        onError("Wrong decryption key provided by user!");
+                        return;
+                    }
+
+                    LocalSecureStorage.prototype._encryptionkey = encryptionKey;
+                    onSuccess();
+                });
+
+            });
+        }, function () {
+            // we don't have an encryption key, create one, and store dummy data with it
+            confirmFromBg("Fill in a new password for the secure storage.. (TODO)", function (encryptionKey) {
+                var verifiers = Keepass.helpers.getVerifier(encryptionKey);
+
+                var data = {};
+                data["local_secure_storage_encryption_key_test"] = {
+                    // data: Keepass.helpers.encrypt("DUMMY_DATA", encryptionKey, verifiers[0]),
+                    nonce: verifiers[0],
+                    verifier: verifiers[1]
+                };
+
+                browser.storage.local.set(data);
+
+                LocalSecureStorage.prototype._encryptionkey = encryptionKey;
+                onSuccess();
+            });
+        });
+    }
+
+};
+
 LocalSecureStorage.prototype.has = function(key, onHas, onNotHas) {
     browser.storage.local.get(key, function (data) {
         if (Object.keys(data).length === 0) {
@@ -14,43 +82,39 @@ LocalSecureStorage.prototype.has = function(key, onHas, onNotHas) {
 };
 
 LocalSecureStorage.prototype.set = function(key, value, done) {
-    this.has(key, function(value) {
+    this._unlockStorage(function () {
+        SecureStorage.prototype._setCache(key, value);
+        var verifiers = Keepass.helpers.getVerifier(LocalSecureStorage.prototype._encryptionkey);
+
+        var data = {};
+        data[key] = {
+            data: Keepass.helpers.encrypt(value, LocalSecureStorage.prototype._encryptionkey, verifiers[0]),
+            nonce: verifiers[0],
+            verifier: verifiers[1]
+        };
+
+        browser.storage.local.set(data);
+
+        done();
+    }, function () {
         // TODO
-    }, function() {
-        confirmFromBg("Fill in a new password for the secure storage.. (TODO)", function (keyToStorage) {
-            SecureStorage.prototype._setCache(key, value);
-            var verifiers = Keepass.helpers.getVerifier(keyToStorage);
-
-            var data = {};
-            data[key] = {
-                data: Keepass.helpers.encrypt(value, keyToStorage, verifiers[0]),
-                nonce: verifiers[0],
-                verifier: verifiers[1]
-            };
-
-            browser.storage.local.set(data);
-
-            done();
-        });
     });
-
-
 };
 
 LocalSecureStorage.prototype.get = function (key, onSuccess, onError) {
-    confirmFromBg("Fill in the existing password for the secure storage.. (TODO)", function (keyToStorage) {
+    this._unlockStorage(function () {
         browser.storage.local.get(key).then(function(data) {
             var actualData = data[key];
             var iv = actualData.nonce;
             var verifier = actualData.verifier;
 
             /**
-             * First verify that the provided key to the LocalSecureStorage is correct.
+             * First verify that the data is encrypted with the key stored in this._encryptionKey.
              */
             var checkIv = slowAES.decrypt(
                 cryptoHelpers.convertStringToByteArray(atob(verifier)),
                 slowAES.modeOfOperation.CBC,
-                cryptoHelpers.convertStringToByteArray(atob(keyToStorage)),
+                cryptoHelpers.convertStringToByteArray(atob(LocalSecureStorage.prototype._encryptionkey)),
                 cryptoHelpers.convertStringToByteArray(atob(iv))
             );
 
@@ -68,7 +132,7 @@ LocalSecureStorage.prototype.get = function (key, onSuccess, onError) {
             var decryptedData = slowAES.decrypt(
                 cryptoHelpers.convertStringToByteArray(atob(actualData.data)),
                 slowAES.modeOfOperation.CBC,
-                cryptoHelpers.convertStringToByteArray(atob(keyToStorage)),
+                cryptoHelpers.convertStringToByteArray(atob(LocalSecureStorage.prototype._encryptionkey)),
                 cryptoHelpers.convertStringToByteArray(atob(iv))
             );
 
@@ -76,6 +140,8 @@ LocalSecureStorage.prototype.get = function (key, onSuccess, onError) {
 
             onSuccess(decryptedDataStr);
         });
+
+    }, function () {
 
     });
 };
--- background/init.js
+++ background/init.js
@@ -1,10 +1,16 @@
 function init() {
-    browser.storage.local.clear();
+    // browser.storage.local.clear(); // uncomment this to test the mechanism to ask the user for a new key
     var ss = new LocalSecureStorage();
     ss.set("test", "abc", function () {
-        console.log("Stored abc")
+        console.log("Stored abc");
+        // the following shouldn't ask for a new or existing key
         ss.get("test", function (data) {
             console.log("Fetched " + data);
+        }, function () {
+            console.log("Couldn't fetch data with key test...");
+        });
+        ss.get("test", function (data) {
+            console.log("Fetched second time " + data);
         }, function () {
             console.log("Couldn't fetch data with key test...");
         });
