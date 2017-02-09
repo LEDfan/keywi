06d01d91f0ec92a7b9aa858bb70e4c7ad4e8b4b8
LEDfan <tobia@ledfan.be>
2017-02-09 17:08:05 +0100

    Show notification when user closes windows

--- background/LocalSecureStorage.prompts.js
+++ background/LocalSecureStorage.prompts.js
@@ -13,13 +13,27 @@ LocalSecureStorage.prompts.setupNewPassword = function () {
                 width: 400,
                 height: 600
             }).then(function(newWindow) {
+                let onRemoved = function(tabId, removeInfo) {
+                    if (tabId == tab.id) {
+                        console.log("Setup storage was aborted!");
+                        browser.notifications.create({
+                            "type": "basic",
+                            "iconUrl": browser.extension.getURL("icons/keepass-96.png"),
+                            "title": "{Keepass}",
+                            "message": "Secure Storage Setup was canceled, {Keepass} cannot work without. You can re-setup it from the settings."
+                        });
+                    }
+                };
+
                 browser.runtime.onMessage.addListener(function _func(request, sender, sendResponse) {
                     if (request.type === "ss_setup_password_user_input") {
                         resolve(request.data.password);
-                        browser.windows.remove(newWindow.id);
                         browser.runtime.onMessage.removeListener(_func);
+                        browser.tabs.onRemoved.removeListener(onRemoved);
+                        browser.windows.remove(newWindow.id);
                     }
                 });
+                browser.tabs.onRemoved.addListener(onRemoved);
             });
         });
 
@@ -39,13 +53,26 @@ LocalSecureStorage.prompts.unlock = function () {
                 width: 400,
                 height: 600
             }).then(function(newWindow) {
+                let onRemoved = function(tabId, removeInfo) {
+                    if (tabId == tab.id) {
+                        console.log("Unlock Secure storage was aborted!");
+                        browser.notifications.create({
+                            "type": "basic",
+                            "iconUrl": browser.extension.getURL("icons/keepass-96.png"),
+                            "title": "{Keepass}",
+                            "message": "Secure Storage unlock was canceled, {Keepass} cannot work without."
+                        });
+                    }
+                };
                 browser.runtime.onMessage.addListener(function _func(request, sender, sendResponse) {
                     if (request.type === "ss_unlock_user_input") {
                         resolve(request.data.password);
-                        browser.windows.remove(newWindow.id);
                         browser.runtime.onMessage.removeListener(_func);
+                        browser.tabs.onRemoved.removeListener(onRemoved);
+                        browser.windows.remove(newWindow.id);
                     }
                 });
+                browser.tabs.onRemoved.addListener(onRemoved);
             });
         });
     });
