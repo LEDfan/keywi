eb92fde76510d4279a2c8d2764943d40f2c0d747
LEDfan <tobia@ledfan.be>
2017-02-12 14:48:43 +0100

    Always load fill-username-and-password.js exactly once

--- background/contextmenu.js
+++ background/contextmenu.js
@@ -15,23 +15,20 @@ browser.runtime.onMessage.addListener((request, sender, sendresponse) => {
             title: "{Keepass}"
         });
     }
-})
+});
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId == "username-and-password") {
         if (activeGetLogins.indexOf(tab.id) === -1) {
             // prevent from simultaneous filling in the credentials
             activeGetLogins.push(tab.id);
-            browser.tabs.executeScript(tab.id, {file: "content_scripts/fill-username-and-password.js", allFrames: true}, function(result) {
-                console.log("Done, now send user and password!");
-                Keepass.getLogins(tab.url, function(entry) {
-                    browser.tabs.sendMessage(tab.id, {
-                        type: "username-and-password",
-                        username: entry.Login,
-                        password: entry.Password
-                    });
-                    activeGetLogins.splice(activeGetLogins.indexOf(tab.id), 1);
+            Keepass.getLogins(tab.url, function(entry) {
+                browser.tabs.sendMessage(tab.id, {
+                    type: "username-and-password",
+                    username: entry.Login,
+                    password: entry.Password
                 });
+                activeGetLogins.splice(activeGetLogins.indexOf(tab.id), 1);
             });
         }
     }
