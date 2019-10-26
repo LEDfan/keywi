0d4d0fd36110aa68732ed1a848294abdf9a87055
Tobia De Koninck <LEDfan@users.noreply.github.com>
2019-10-26 17:17:31 +0200

    Add copyright note

--- background/lib/KeepassXCBackend.js
+++ background/lib/KeepassXCBackend.js
@@ -1,3 +1,28 @@
+/**
+ * @copyright (C) 2019 Tobia De Koninck
+ *
+ * This file is part of Keywi.
+ * Keywi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Keywi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Keywi.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Note: parts of this file are directly based on code of the keepassxreboot/keepassxc-browser project.
+ * (Especially the following file: https://github.com/keepassxreboot/keepassxc-browser/blob/develop/keepassxc-browser/background/keepass.js)
+ * That project is licensed under the GPL-3.0 (like Keywi).
+ * We want to thank the developers and contributors of the above project.
+ *
+ */
+
 class KeepassXCBackend extends PasswordBackend {
 
   /**
